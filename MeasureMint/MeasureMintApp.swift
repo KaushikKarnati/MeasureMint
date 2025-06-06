f914807edf72b2660babf4e6923e40b797ccd0ec
KaushikKarnati <kaushik.karnati@icloud.com>
2025-06-05 19:43:04 -0500

    Initial Commit

---
+++ MeasureMint/MeasureMintApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureMintApp.swift
+//  MeasureMint
+//
+//  Created by kaushik on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct MeasureMintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeasureMintTests/MeasureMintTests.swift
@@ -0,0 +1,17 @@
+//
+//  MeasureMintTests.swift
+//  MeasureMintTests
+//
+//  Created by kaushik on 6/5/25.
+//
+
+import Testing
+@testable import MeasureMint
+
+struct MeasureMintTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
